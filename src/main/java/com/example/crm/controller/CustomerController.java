3a226abd22138466036473e8fc25318d0ad01efe
Nikita Kezin <nik77kez@gmail.com>
2022-06-11 12:14:38 +0300

    Created Service Layer

--- src/main/java/com/example/crm/controller/CustomerController.java
+++ src/main/java/com/example/crm/controller/CustomerController.java
@@ -1,25 +1,25 @@
 package com.example.crm.controller;
 
-import com.example.crm.dao.CustomerDAO;
 import com.example.crm.entity.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.example.crm.service.CustomerService;
 
 import java.util.List;
 
 @Controller
 public class CustomerController {
 
-    //need to inject the customer dao
+    //need to inject our customer com.example.crm.service
     @Autowired
-    private CustomerDAO customerDAO;
+    private CustomerService customerService;
 
     @GetMapping("/")
     public String listCustomers(Model theModel) {
-        //get customers from the dao
-        List<Customer> customers = customerDAO.getCustomers();
+        //get customers from the com.example.crm.service
+        List<Customer> customers = customerService.getCustomers();
         //add the customers to the model
         theModel.addAttribute("customers", customers);
         return "listCustomers";
