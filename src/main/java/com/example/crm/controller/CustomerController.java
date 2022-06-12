fc05df18735c3d0786c4384bfad2b2b7c76a15c5
Nikita Kezin <nik77kez@gmail.com>
2022-06-12 13:09:20 +0300

    Create feature to add a customer

--- src/main/java/com/example/crm/controller/CustomerController.java
+++ src/main/java/com/example/crm/controller/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.example.crm.service.CustomerService;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -25,4 +27,21 @@ public class CustomerController {
         return "listCustomers";
     }
 
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+
+        //create model attribute to bind form data
+        Customer customer = new Customer();
+        model.addAttribute("customer", customer);
+        return "customerForm";
+    }
+
+    @PostMapping("/saveCustomer")
+    public String saveCustomer(@ModelAttribute("customer") Customer customer) {
+
+        //save customer using our service
+        customerService.saveCustomer(customer);
+
+        return "redirect:/";
+    }
 }
