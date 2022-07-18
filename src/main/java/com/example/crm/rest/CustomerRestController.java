731c0ca22e00df1696b873ac8713b443e3be9d19
Nikita Kezin <nik77kez@gmail.com>
2022-07-18 20:24:58 +0300

    Add Post method addCustomer

--- src/main/java/com/example/crm/rest/CustomerRestController.java
+++ src/main/java/com/example/crm/rest/CustomerRestController.java
@@ -3,10 +3,7 @@ package com.example.crm.rest;
 import com.example.crm.entity.Customer;
 import com.example.crm.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +25,13 @@ public class CustomerRestController {
         if (customer == null) {
             throw new CustomerNotFoundException("Customer id not found - " + customerId);
         }
+        return customer;
+    }
+
+    @PostMapping("/customers")
+    public Customer addCustomer(@RequestBody Customer customer) {
+        customer.setId(0);
+        customerService.saveOrUpdateCustomer(customer);
         return customer;
     }
 
